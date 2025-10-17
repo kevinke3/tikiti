b8703323fea38a95894e2f9407b2a4d984f2ccea
kevinke3 <kevinke386@gmail.com>
2025-10-17 14:56:31 +0300

    updates

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 # app.py
 from flask import Flask, render_template, request, jsonify, session
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS  # Add this import
 from werkzeug.security import generate_password_hash, check_password_hash
 import qrcode
 import os
 from datetime import datetime
 import random
 import string
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'tikozetu-secret-key-2023'
